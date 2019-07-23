c4f8da5bd451c05a9f7db7f43cfce9eca1a2a586
Or Milis <ormilism@gmail.com>
2019-07-23 03:23:11 +0300

    Added search bar

--- Dali-ios/SearchViewController.swift
+++ Dali-ios/SearchViewController.swift
@@ -16,6 +16,8 @@ class SearchViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        searchField.setPadding()
+        searchField.setBottomBorder()
 
         // Do any additional setup after loading the view.
     }
@@ -33,20 +35,49 @@ class SearchViewController: UIViewController {
     
     @IBAction func onTextChange(_ sender: UITextField) {
         self.buffTimer.invalidate()
+        self.buffTimer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: false, block: { _ in
+            self.sendSearchText()
+        })
+    }
+    
+    func sendSearchText() {
         print("In")
         guard let text = searchField.text else { return }
         if(text == "") { return }
-        self.buffTimer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: false, block: { _ in
-            RestController.searchUsers(str: text, completion: { (res) in
-                switch res {
-                case .success(let genData):
-                    genData.forEach({ (artist) in
-                        print(artist.name)
-                    })
-                case .failure(let err):
-                    print(err)
-                }
-            })
+        RestController.searchUsers(str: text, completion: { (res) in
+            switch res {
+            case .success(let genData):
+                genData.forEach({ (artist) in
+                    print(artist.name)
+                })
+            case .failure(let err):
+                print(err)
+            }
         })
+    }
+}
+
+extension SearchViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        self.searchField.resignFirstResponder()
+        self.buffTimer.invalidate()
+        self.sendSearchText()
+        return true
+    }
+}
+
+extension UITextField {
+    func setPadding() {
+        let paddingView = UIView(frame: CGRect(x: 0.0, y: 0.0, width: 10, height: self.frame.height))
+        self.leftView = paddingView
+        self.leftViewMode = .always
+    }
+    
+    func setBottomBorder() {
+        let bottomLine = CALayer()
+        bottomLine.frame = CGRect.init(x: 0, y: self.frame.size.height - 1, width: self.frame.size.width, height: 1)
+        bottomLine.backgroundColor = UIColor.darkGray.cgColor
+        self.borderStyle = UITextField.BorderStyle.none
+        self.layer.addSublayer(bottomLine)
     }
 }
